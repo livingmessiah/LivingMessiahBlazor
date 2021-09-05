79fb836a4f30487972b95ff1fd90eff964afea02
LivingMessiahJohn <LivingMessiahJohn@outlook.com>
2021-09-04 17:08:06 -0700

    Fixed tabs for /BloodMoons &  /Further (Further Studies) Deleted LivingMessiah.Web\Domain\BookTitle.cs Deleted LivingMessiah.Web\Data\Book.cs Trying out modal (See Pages/BlazorExamples/

---
+++ LivingMessiah.Web/Pages/BlazorExamples/YeshuaInOT.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LivingMessiah.Web.Pages.BlazorExamples
+{
+	public partial class YeshuaInOT
+	{
+	}
+}
---
+++ LivingMessiah.Web/Pages/OtherPages/FurtherStudies.razor.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace LivingMessiah.Web.Pages.OtherPages
+{
+	public partial class FurtherStudies
+	{
+		public bool IsCollapsed { get; set; } = true;
+		public int VerseId { get; set; }
+		public string ButtonText { get; set; } = "Details";
+		public string ButtonChevron { get; set; } = " fas fa-chevron-down";
+
+		protected void ToggleVersePopupClick(bool isCollapsed, int verseId)
+		{
+			IsCollapsed = !isCollapsed;
+			VerseId = verseId;
+			ButtonText = IsCollapsed ? "Details" : "Hide";
+			ButtonChevron = IsCollapsed ? "fas fa-chevron-down" : "fas fa-chevron-up";
+		}
+	}
+}
