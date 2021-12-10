f796cf327ece43b84ac45835eeb4fa0090b80d05
LivingMessiahJohn <LivingMessiahJohn@outlook.com>
2021-12-10 12:37:40 -0700

    Stashing for now. Need to test a new see BlazorImageUpload-MudBlazor-MediatR.md but use SyncFusion insteat

---
+++ LivingMessiah.Web/Pages/UpcomingEvents/Upload/UploadComponent.razor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using static LivingMessiah.Web.Services.Auth0;
+using Syncfusion.Blazor.Inputs;
+
+namespace LivingMessiah.Web.Pages.UpcomingEvents.Upload
+{
+	//[Authorize(Roles = Roles.AdminOrAnnouncements)]
+	public partial class UploadComponent
+	{
+		//[Inject]
+		//public ILogger<UploadComponent> Logger { get; set; }
+
+		[Parameter]
+		public int Id { get; set; }
+
+		public void OnFileRemove(RemovingEventArgs args)
+		{
+			args.PostRawFile = false;
+		}
+
+	}
+}
