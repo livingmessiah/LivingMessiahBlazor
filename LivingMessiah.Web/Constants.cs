7a7fca3ba176674e5a0ec1bd637e094f0f27e8c3
JohnMarsing <johnmarsing@gmail.com>
2022-03-20 09:30:13 -0700

    Created YouTubeFeed component and incorporated it in Admin WeeklyVideos - Add nuget pkg System.ServiceModel.Syndication

--- LivingMessiah.Web/Constants.cs
+++ LivingMessiah.Web/Constants.cs
@@ -153,3 +153,26 @@ public static class Emails
 		}
 
 }
+
+
+public static class SocialMedia
+{
+	public static class YouTube
+	{
+		private const string _channelId = "UCz_q3-dBtU_sSbEojRP57OQ";
+		private const string _baseFeedUrl = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+		private const string _baseNormalUrl = "https://www.youtube.com/channel/";
+
+		public static string YouTubeFeed()
+		{
+			return _baseFeedUrl + _channelId;
+		}
+
+		public static string YouTubeNormal()
+		{
+			return _baseNormalUrl + _channelId;
+		}
+
+	}
+}
+
