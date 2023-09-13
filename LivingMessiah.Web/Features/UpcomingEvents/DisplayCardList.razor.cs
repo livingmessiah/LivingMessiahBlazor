1b11d80dc1dc477b9c329517cfe377225febee77
JohnMarsing <johnmarsing@gmail.com>
2023-09-12 19:54:34 -0700

    Created DisplayCardList and moved most logic from Index to there.

---
+++ LivingMessiah.Web/Features/UpcomingEvents/DisplayCardList.razor.cs
@@ -0,0 +1,37 @@
+﻿using Blazored.Toast.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Components;
+
+using Page = LivingMessiah.Web.Links.UpcomingEvents;
+
+namespace LivingMessiah.Web.Features.UpcomingEvents;
+
+public partial class DisplayCardList
+{
+	[Inject] public ILogger<DisplayCardList>? Logger { get; set; }
+	[Inject] public SpecialEvents.Data.IRepository? db { get; set; }
+	[Inject] public IToastService? Toast { get; set; }
+
+	readonly string inside = $"page {Page.Index}; class:{nameof(DisplayCardList)}";
+
+	protected List<SpecialEvents.FormVM> SpecialEvents = new();
+
+	protected override async Task OnInitializedAsync()
+	{
+		Logger!.LogDebug(string.Format("Inside {0}!{1}", inside, nameof(OnInitializedAsync)));
+		try
+		{
+			SpecialEvents = await db!.GetCurrentEvents();
+		}
+		catch (Exception ex)
+		{
+			Logger!.LogError(ex, string.Format("...Inside catch of {0}", inside + "!" + nameof(OnInitializedAsync)));
+			Toast!.ShowError($"{Global.ToastShowError}; inside: {inside}!{nameof(OnInitializedAsync)}");
+		}
+	}
+
+
+}
